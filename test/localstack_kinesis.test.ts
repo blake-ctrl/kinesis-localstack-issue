2dfbf71b2951465852b4790dd9e27dc65aea51db
Blake Livingston <blake@ctrl.llc>
2022-08-25 12:46:45 -0400

    Initial commit

---
+++ test/localstack_kinesis.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LocalstackKinesis from '../lib/localstack_kinesis-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/localstack_kinesis-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LocalstackKinesis.LocalstackKinesisStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
