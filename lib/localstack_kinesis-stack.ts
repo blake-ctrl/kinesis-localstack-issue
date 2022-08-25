f932cfc7c74821f6b39156b318ec4e32a7fc6ffd
Blake Livingston <blake@ctrl.llc>
2022-08-25 12:54:15 -0400

    Manifest kinesis stream name issue

--- lib/localstack_kinesis-stack.ts
+++ lib/localstack_kinesis-stack.ts
@@ -1,4 +1,5 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { Stream } from 'aws-cdk-lib/aws-kinesis';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,7 @@ export class LocalstackKinesisStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LocalstackKinesisQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const kstream = new Stream(this, "KinesisStream");
+    new CfnOutput(this, "KinesisStreamName", {value: kstream.streamName});
   }
 }
